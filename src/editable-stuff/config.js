232de0495207800543f5f2914f647b74dc4390e7
ockap <62844331+slewinus@users.noreply.github.com>
2022-10-21 13:55:59 +0200

    comite #2

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -33,7 +33,7 @@ const about = {
   heading: "About Me",
   imageSize: 375,
   message:
-    "I was born on April 30, 2003, in Saint-Cloud, a suburb of Paris. At a young age, I started to be interested in cutting-edge technology and how they could revolutionize our world. Thanks to forums and documentation, I learned different programming languages such as Python, HTML/CSS and IoT. I got seriously interested in computer networks and I wanted to understand how it was possible to secure them. Quickly I started to work in this field and at at my 18th birthday I created my own company",
+    "I was born on April 30, 2003, in Saint-Cloud, a suburb of Paris. At a young age, I started to be interested in cutting-edge technology and how they could revolutionize our world. Thanks to forums and documentation, I learned different programming languages such as Python, HTML/CSS and IoT. I got seriously interested in computer networks and I wanted to understand how it was possible to secure them. Quickly I started to work in this field and at my 18th birthday I created my own company",
   resume: require("../editable-stuff/resume.pdf"),
 };
 
